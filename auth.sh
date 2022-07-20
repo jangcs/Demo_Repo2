117578194e5de1b98395e497e1b0b8ae5fee0af0
Choulsoo Jang from CR-EDGE <jangcs@gmail.com>
2022-07-20 15:45:58 +0900

    auth.sh

---
+++ auth.sh
@@ -0,0 +1,4 @@
+gcloud auth activate-service-account cr-model@gcp-test-710302.iam.gserviceaccount.com --key-file="/home/jangcs/dvc-gs/gcp-test-710302-880fc36e6747.json"
+
+# rm "/home/jangcs/dvc-gs/gcp-test-710302-880fc36e6747.json"
+
